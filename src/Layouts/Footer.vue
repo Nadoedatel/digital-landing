1fa4efcd7839b5579feed0ddd2e8901795bb4663
Nikita <nktvrntsv@gmail.com>
2025-09-18 19:08:58 +0300

    end first page
    add new section
    - footer
    - thankYou

--- src/Layouts/Footer.vue
+++ src/Layouts/Footer.vue
@@ -1,11 +1,147 @@
 <script setup lang="ts">
 
+import Header from "@/Layouts/Header.vue";
+import Button from "@/components/ui/Button/Button.vue";
 </script>
 
 <template>
-
+  <footer class="container">
+    <div class="footer__container-nav">
+      <div class="logo">
+        <div>
+          <img src="../../public/Logo.svg" alt="logo" />
+        </div>
+        <div>
+          <img src="../assets/images/SquareUp.svg" alt="squareUp" />
+        </div>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <RouterLink to="/">Home</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="services">Services</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="work">Work</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="process">Process</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="about">About</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="careers">Careers</RouterLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <div class="footer">
+          <h3>Stay Connected</h3>
+          <img src="@/assets/images/Footer/Facebook.svg" alt="facebook" />
+          <img src="@/assets/images/Footer/Twitter.svg" alt="twitter" />
+          <img src="@/assets/images/Footer/LinkedIn.svg" alt="LinkedIn" />
+        </div>
+      </div>
+    </div>
+    <div class="footer__container-info">
+      <div class="footer__contact">
+        <div class="footer__contact-item">
+          <img src="@/assets/images/Footer/Mail.svg" alt="mail" />
+          <p>hello@squareup.com</p>
+        </div>
+        <div class="footer__contact-item">
+          <img src="@/assets/images/Footer/Phone.svg" alt="phone" />
+          <p>+91 91813 23 2309</p>
+        </div>
+        <div class="footer__contact-item">
+          <img src="@/assets/images/Footer/Location.svg" alt="location" />
+          <p>Somewhere in the World</p>
+        </div>
+      </div>
+      <p>© 2023 SquareUp. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 50px 162px;
+  gap: 50px;
+}
 
+.footer__container-info {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+}
+.footer__container-nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+}
+
+nav ul{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+ul li {
+  background: none;
+  border: none;
+  color: var(--vt-c-text-light);
+  cursor: pointer;
+  padding: 14px 24px;
+  border-radius: 8px;
+}
+
+ul li:hover {
+  background-color: var(--vt-c-primary-hover);
+}
+
+.footer {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 14px 24px;
+  border: 1px solid var(--vt-c-text-dark);
+  border-radius: 12px;
+  gap: 20px;
+}
+
+.footer__contact {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+}
+
+.footer__contact-item {
+  padding: 16px;
+  border-bottom: 1px solid var(--vt-c-gray-15);
+}
 </style>
--- src/components/home/ThankYou.vue
+++ src/components/home/ThankYou.vue
@@ -1,33 +1,73 @@
 <script setup lang="ts">
-
 import Button from "@/components/ui/Button/Button.vue";
+import {ref} from "vue";
+const selected = ref<string[]>([]);
 </script>
 
 <template>
   <section>
-    <div class="thank-container">
+    <div class="thank__container">
       <img src="/Logo.svg" alt="Logo" />
-      <div class="thank-title">
+      <div class="thank__title">
         <h1 class="">Thank you for your Interest in SquareUp.</h1>
         <p class="">We would love to hear from you and discuss how we can help bring your digital ideas to life. Here are the different ways you can get in touch with us.</p>
       </div>
       <Button color="green">Start Project</Button>
     </div>
-    <div class="">
-      <div class="">
-        <div class="">
-          <div>
-            <h3></h3>
-            <p></p>
+    <div class="thank__wrapper">
+      <div class="thank__container-items">
+        <div class="thank__item">
+          <p>Full Name</p>
+          <input type="text" placeholder="Type here" />
+        </div>
+        <div class="thank__item">
+          <p>Email</p>
+          <input type="email" placeholder="Type here" />
+        </div>
+        <div class="thank__item wide">
+          <p>Why are you contacting us?</p>
+          <div class="checkbox-group">
+            <label class="checkbox">
+              <input type="checkbox" v-model="selected" value="Web Design" />
+              <span class="custom"></span>
+              <span class="text">Web Design</span>
+            </label>
+
+            <label class="checkbox">
+              <input type="checkbox" v-model="selected" value="Mobile App Design" />
+              <span class="custom"></span>
+              <span class="text">Mobile App Design</span>
+            </label>
+
+            <label class="checkbox">
+              <input type="checkbox" v-model="selected" value="Collaboration" />
+              <span class="custom"></span>
+              <span class="text">Collaboration</span>
+            </label>
+
+            <label class="checkbox">
+              <input type="checkbox" v-model="selected" value="Others" />
+              <span class="custom"></span>
+              <span class="text">Others</span>
+            </label>
           </div>
         </div>
+        <div class="thank__item wide">
+          <p>Your Budget</p>
+          <p>Slide to indicate your budget range</p>
+        </div>
+        <div class="thank__item wide">
+          <p>Your Message</p>
+          <input type="text" placeholder="Type here" />
+        </div>
+        <Button class="wide__btn" color="green">Submit</Button>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-.thank-container {
+.thank__container {
   background: url("@/assets/images/HomePage/ThankYou/ThankYouBg.svg") no-repeat;
   background-size: cover;
   padding: 120px 350px;
@@ -39,9 +79,101 @@ import Button from "@/components/ui/Button/Button.vue";
   gap: 50px;
 }
 
-.thank-title {
+.thank__title {
   display: flex;
   flex-direction: column;
   gap: 14px;
 }
+
+.wide {
+  grid-column: 1 / -1;
+}
+
+.wide__btn {
+  grid-column: 1/ -1;
+  justify-self: center;
+}
+
+.thank__container-items {
+  display: grid;
+  padding: 80px 80px;
+  grid-template-columns: 1fr 1fr;
+  gap: 40px;
+  border: 1px solid var(--vt-c-gray-15);
+}
+
+.thank__container-items p {
+  font-size: 22px;
+}
+
+.thank__wrapper {
+  padding: 0 268px;
+}
+
+.thank__item {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: rgba(36, 36, 36, 0.5);
+  border-radius: 8px;
+  padding: 24px 40px;
+  border: 1px solid var(--vt-c-gray-15);
+}
+
+.thank__item input {
+  color: var(--vt-c-gray-40);
+  background-color: rgba(0, 0, 0, 0);
+  outline: none;
+  border: none;
+  padding-bottom: 6px;
+  border-bottom:1px solid var(--vt-c-gray-40);
+}
+
+.checkbox-group {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px 40px;
+}
+
+.checkbox {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  cursor: pointer;
+  user-select: none;
+  font-size: 18px;
+  color: #fff;
+}
+
+.checkbox input {
+  display: none;
+}
+
+.checkbox .custom {
+  width: 24px;
+  height: 24px;
+  border: 2px solid rgba(255, 255, 255, 0.2);
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.3s, background 0.3s;
+  position: relative;
+}
+
+.checkbox .custom::after {
+  content: "";
+  width: 6px;
+  height: 12px;
+  border-right: 2px solid var(--vt-c-green-50);
+  border-bottom: 2px solid var(--vt-c-green-50);
+  transform: rotate(45deg);
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+
+.checkbox input:checked + .custom::after {
+  opacity: 1;
+}
+
 </style>
